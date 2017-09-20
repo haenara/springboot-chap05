caaa850c8f37fbf15bb86617c46c062a78cb0832
java <java@bigdata>
2017-09-20 11:36:42 +0900

    first commit

---
+++ src/main/java/com/example/controller/SampleController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.java.Log;
+
+@Controller
+@Log
+public class SampleController {
+
+	@GetMapping("/thymeleaf")
+	public String thymeleaf(){
+		log.info("/thymeleaf call");
+		
+		return "thymeleaf/hello";
+		
+	}
+	
+	@GetMapping("/jsp")
+	public String jsp(){
+		log.info("/jsp call");
+		return "jsp/hello";
+	}
+	
+	
+	
+}
